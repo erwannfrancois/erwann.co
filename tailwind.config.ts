81aff6b04650a4560788a37a8214aebd3c1d91e4
Erwann <mail@erwann.co>
2024-04-12 15:14:46 +0200

    Theme set up

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,59 @@
 import type { Config } from "tailwindcss";
 
-const config: Config = {
+const config = {
+  darkMode: ["class"],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontSize: {
+        xs: ["14px", "16px"],
+        sm: ["16px", "22px"],
+        base: ["18px", "24px"],
+        lg: ["22px", "28px"],
+        xl: ["24px", "32px"],
+        display: ["48px", "54px"],
+      },
+      colors: {
+        border: {
+          DEFAULT: "rgb(var(--border) / <alpha-value>)",
+          secondary: "rgb(var(--border-secondary) / <alpha-value>)",
+        },
+        surface: {
+          DEFAULT: "rgb(var(--surface-default) / <alpha-value>)",
+          inverse: "rgb(var(--surface-inverse) / <alpha-value>)",
+          secondary: "rgb(var(--surface-secondary) / <alpha-value>)",
+          accent_primary: "rgb(var(--surface-accent-primary) / <alpha-value>)",
+          accent_secondary:
+            "rgb(var(--surface-accent-secondary) / <alpha-value>)",
+        },
+        foreground: {
+          DEFAULT: "rgb(var(--foreground-default) / <alpha-value>)",
+          inverse: "rgb(var(--foreground-inverse) / <alpha-value>)",
+          secondary: "rgb(var(--foreground-secondary) / <alpha-value>)",
+          tertiary: "rgb(var(--foreground-tertiary) / <alpha-value>)",
+          on_accent: "rgb(var(--foreground-on-accent) / <alpha-value>)",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
     },
   },
-  plugins: [],
-};
+} satisfies Config;
+
 export default config;
