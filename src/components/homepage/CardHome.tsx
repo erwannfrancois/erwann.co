02018d63a3dcee18cc9802b1faee4a956b56f475
Erwann <mail@erwann.co>
2024-04-18 22:07:08 +0200

    Correcting Dark mode

--- src/components/homepage/CardHome.tsx
+++ src/components/homepage/CardHome.tsx
@@ -10,7 +10,7 @@ export default function CardHome({ children, color }: CardHomeProps) {
   return (
     <div
       className={cn(
-        "min-h-[400px] w-full flex items-center justify-center mb-10 break-inside-avoid-column shadow-md px-8 md:px-12 py-10 rounded-xl",
+        "min-h-[400px] w-full flex items-center justify-center dark:text-foreground-inverse mb-10 break-inside-avoid-column shadow-md px-8 md:px-12 py-10 rounded-xl",
         color
       )}
     >
