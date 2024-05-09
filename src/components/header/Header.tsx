4994510b443d7e79e607f8c78f2d8e473ca4e34d
Erwann <mail@erwann.co>
2024-05-09 10:22:25 +0200

    Changed Favicon

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -13,28 +13,15 @@ export default function Header() {
           <div className="flex flex-row space-x-4">
             <Link
               href="/"
-              className="flex flex-row space-x-2 font-medium hover:underline hover:underline-offset-4"
+              className="flex flex-row space-x-1.5 font-medium hover:underline hover:underline-offset-4"
             >
               <div className="w-5 h-5 text-foreground-primary">
                 {/* Logo */}
-                <svg
-                  width="20"
-                  height="20"
-                  viewBox="0 0 20 20"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <g clipPath="url(#clip0_101_239)">
-                    <path
-                      d="M12.4125 6.63412L13.3232 -0.818377L11.4133 -0.85621L9.65495 5.70793C9.02118 7.76015 8.02277 8.16578 5.91875 8.27973L0.92144 8.47126L0.558367 10.5703L4.91347 10.9575C7.44944 11.1738 8.14048 12.5363 7.95807 14.3277L7.10955 21.2522L9.08171 21.2913L10.9421 14.117C11.3842 12.3412 12.3389 11.3017 14.2171 11.3078L19.4843 11.1216L19.8014 9.32254L14.311 8.71574C13.0943 8.57751 12.2333 8.06242 12.4223 6.60318L12.4125 6.63412Z"
-                      fill="currentColor"
-                    />
-                  </g>
-                  <defs>
-                    <clipPath id="clip0_101_239">
-                      <rect width="20" height="20" fill="white" />
-                    </clipPath>
-                  </defs>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+                  <path
+                    fill="currentColor"
+                    d="M15.24,7.42l-4.41-.88c-.66-.12-.76-.47-.77-1.22l.04-4.59c-.13-.64-1.1-.64-1.22,0l-1.9,4.59c-.3.71-1.16,1.11-1.82,1.22l-4.41.88c-.68.12-.68,1.04,0,1.16l4.41.88c.66.12.91.56.91,1.22l-.04,4.59c.13.64,1.1.64,1.22,0l1.9-4.59c.3-.73,1.02-1.11,1.68-1.22l4.41-.88c.68-.12.68-1.04,0-1.16Z"
+                  />
                 </svg>
               </div>
 
