34834078b941721333ecce256393ca0e11e18e5e
Erwann <mail@erwann.co>
2024-05-16 18:17:24 +0200

    Cleaning GIFs

--- src/lib/utilsInterface.ts
+++
@@ -1,6 +0,0 @@
-export function copyMailToClipboard() {
-  const textToCopy = "mail@erwann.co";
-  navigator.clipboard
-    .writeText(textToCopy)
-    .catch((error) => console.error("Error copying text: ", error));
-}
